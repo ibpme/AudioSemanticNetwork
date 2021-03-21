07de0fd6f77c0b98d6abaa151801984cd7c98267
Iman-Budi Pranakaish <ibpranakasih@gmail.com>
2021-03-21 20:01:14 +0700

    add remove short words

--- DataFetching/text_preprocess.py
+++ DataFetching/text_preprocess.py
@@ -11,11 +11,15 @@ stopwords.update(stop_factory.get_stop_words())
 # Use NLTK for Word Distribution and Most Common Words
 
 
-def text_clean(text, case_folding=True, bad_symbols=True, bad_brackets=True, remove_stopwords=True):
+def text_clean(text, case_folding=True, bad_symbols=True, bad_brackets=True, remove_stopwords=True, remove_numbers=False):
     print("Cleaning Text...")
     # lowercase text
     if case_folding:
         text = text.lower()
+
+    if remove_numbers:
+        # Use Regex
+        raise NotImplemented
 
     if bad_brackets:
         # replace REPLACE_BY_SPACE_RE symbols by space in text
@@ -39,6 +43,21 @@ def text_clean(text, case_folding=True, bad_symbols=True, bad_brackets=True, rem
     return text
 
 
+def remove_short_words(text, word_length=3):
+    print("Removing short words from Text...")
+    text_list = text.split()
+    text_list_copy = text_list.copy()
+    words_removed = []
+    for word in text_list_copy:
+        if len(word) <= word_length:
+            words_removed.append(word)
+            text_list.remove(word)
+
+    print("Words removed", words_removed, len(words_removed))
+
+    return " ".join(text_list)
+
+
 def text_stem(text):
     factory = StemmerFactory()
     stemmer = factory.create_stemmer()
@@ -51,13 +70,19 @@ def text_stem(text):
     return stemmed_text
 
 
-with open("file.txt", "r") as file:
-    text = file.read()
+def main():
+    with open("file.txt", "r") as file:
+        text = file.read()
+    cleaned_text = text_clean(text)
+    stemmed_text = text_clean(text_stem(cleaned_text))
+    shorted_text = remove_short_words(stemmed_text)
+    # print(shorted_text)
+    word_tokens = nltk.tokenize.word_tokenize(shorted_text)
+    # print(word_tokens)
+    # word_count = nltk.FreqDist(word_tokens)
+    # print("Most common words :", word_count.most_common())
+    return word_tokens
 
-cleaned_text = text_clean(text)
-stemmed_text = text_clean(text_stem(cleaned_text))
-print(stemmed_text)
-word_tokens = nltk.tokenize.word_tokenize(stemmed_text)
-print(word_tokens)
-word_count = nltk.FreqDist(word_tokens)
-print("Most common words :", word_count.most_common())
+
+if __name__ == "__main__":
+    main()
